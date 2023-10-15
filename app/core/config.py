a5db9ea415f01100312195ecf9691b7b89d6378e
Alexey Potapov <2furius@gmail.com>
2023-10-15 23:34:06 +0300

    first commit

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_title: str
+    app_author: str
+    app_description: str
+    database_url: str
+
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
