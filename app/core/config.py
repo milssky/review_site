a49664e81f4aa7e95def969907dda22e53ae8c82
Alexey Potapov <2furius@gmail.com>
2023-10-28 12:14:21 +0300

    Users subsystem

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_title: str
     app_description: str
     database_url: str
+    secret: str = 'SuperSecretString!'
 
     class Config:
         env_file = ".env"
