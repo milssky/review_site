29f769222bc3368d301f7e37437d8e38375539c6
hikary-dev <tronin371@gmail.com>
2023-12-17 13:58:31 +0300

    Add Docker and Postgress type to models

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.11
+
+RUN apt-get update && \
+  apt-get install -y pkg-config default-libmysqlclient-dev --no-install-recommends \
+  build-essential && \
+  pip install --no-cache-dir poetry 
+
+RUN poetry config virtualenvs.create false
+
+WORKDIR /app
+COPY ./pyproject.toml ./
+RUN poetry install 
+
+COPY ./ ./
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/models/task.py
+++ app/models/task.py
@@ -1,82 +1,68 @@
-import json
-
-from sqlalchemy import (Boolean, Column, DateTime, ForeignKey, String, Table,
-                        TypeDecorator)
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, String, Table
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
 from app.core.db import Base
 
 
-class ArrayType(TypeDecorator):
-    impl = String
-
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            return json.dumps(value)
-
-    def process_result_value(self, value, dialect):
-        if value is not None:
-            return json.loads(value)
-
-
 class Task(Base):
     name = Column(String(50), unique=True)
     text = Column(String(1000))
-    language = Column(ArrayType(100))
-    course_id = mapped_column(ForeignKey("course.id"))
+    language = Column(ARRAY(String))
+    course_id = mapped_column(ForeignKey('course.id'))
 
-    course: Mapped["Course"] = relationship(
-        back_populates="tasks",
-        lazy="selectin",
+    course: Mapped['Course'] = relationship(
+        back_populates='tasks',
+        lazy='selectin',
     )
-    solutions: Mapped[list["Solution"]] = relationship(
-        back_populates="task",
-        lazy="selectin",
+    solutions: Mapped[list['Solution']] = relationship(
+        back_populates='task',
+        lazy='selectin',
     )
 
 
 class Solution(Base):
-    author_id = mapped_column(ForeignKey("user.id"))
-    task_id = mapped_column(ForeignKey("task.id"))
+    author_id = mapped_column(ForeignKey('user.id'))
+    task_id = mapped_column(ForeignKey('task.id'))
     solved_date = Column(DateTime(timezone=True), server_default=func.now())
     status = Column(Boolean(), default=False)
 
-    task: Mapped["Task"] = relationship(
-        back_populates="solutions",
-        lazy="selectin",
+    task: Mapped['Task'] = relationship(
+        back_populates='solutions',
+        lazy='selectin',
     )
-    author: Mapped["User"] = relationship(
-        back_populates="solutions",
-        lazy="selectin",
+    author: Mapped['User'] = relationship(
+        back_populates='solutions',
+        lazy='selectin',
     )
-    files: Mapped[list["File"]] = relationship(
-        back_populates="solution",
-        lazy="selectin",
+    files: Mapped[list['File']] = relationship(
+        back_populates='solution',
+        lazy='selectin',
     )
 
 
 class File(Base):
     name = Column(String(500))
     content = Column(String(1000))
-    solution_id = mapped_column(ForeignKey("solution.id"))
+    solution_id = mapped_column(ForeignKey('solution.id'))
 
-    solution: Mapped["Solution"] = relationship(
-        back_populates="files",
-        lazy="selectin",
+    solution: Mapped['Solution'] = relationship(
+        back_populates='files',
+        lazy='selectin',
     )
 
 
 UserSolutions = Table(
-    "usersolutions",
+    'usersolutions',
     Base.metadata,
-    Column("task_id", ForeignKey("task.id")),
-    Column("user_id", ForeignKey("user.id")),
-    Column("solution_id", ForeignKey("solution.id")),
+    Column('task_id', ForeignKey('task.id')),
+    Column('user_id', ForeignKey('user.id')),
+    Column('solution_id', ForeignKey('solution.id')),
 )
 
 
 class UserTask(Base):
-    task_id = mapped_column(ForeignKey("task.id"))
-    user_id = mapped_column(ForeignKey("user.id"))
+    task_id = mapped_column(ForeignKey('task.id'))
+    user_id = mapped_column(ForeignKey('user.id'))
     is_solved = Column(Boolean(), default=False)
