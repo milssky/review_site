c29b4a6d2e6ffbd2511fbefc3eae3aed2df13661
Alexey Potapov <2furius@gmail.com>
2024-03-02 18:57:35 +0300

    CORS allow all

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import main_router
 from app.core.config import settings
@@ -17,6 +18,14 @@ app = FastAPI(
     title=settings.app_title,
     description=settings.app_description,
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(main_router)
